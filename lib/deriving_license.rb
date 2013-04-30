81117e9fece914dd4e50d957b6238ab73c6a2148
Tom Allen <tom.allen@bigcommerce.com>
2013-04-30 16:43:38 +1000

    Improve output considerably. First pass at license description list and lookup.

--- lib/deriving_license.rb
+++ lib/deriving_license.rb
@@ -2,6 +2,24 @@ require "gemnasium/parser"
 require "bundler"
 
 class DerivingLicense
+  
+  attr_reader :license_details, :license_aliases
+  
+  # TODO: Scrape http://www.gnu.org/licenses/license-list.html#SoftwareLicenses 
+  # and auto-magically generate these details.
+  @@license_details = {
+    # key -> hash of (Name, Link, [Tags]), where tags is an array that may include [:gpl_compatible, :copyleft_compatible, :has_restrictions]
+    "GPL" => {name:"GNU General Public License",link:"http://en.wikipedia.org/wiki/GNU_General_Public_License",tags:[:gpl_compatible, :copyleft_compatible, :has_restrictions]},
+    "MIT" => {name:"Expat License",link:"http://directory.fsf.org/wiki/License:Expat",tags:[:gpl_compatible, :has_restrictions]},
+    "BSD" => {name:"FreeBSD Copyright",link:"http://www.freebsd.org/copyright/freebsd-license.html",tags:[:gpl_compatible, :copyleft_compatible, :has_restrictions]}
+  }
+
+  @@license_aliases = {
+    # hash of names to keys of the license in the master list.
+    "FreeBSD" => "BSD",
+    "Expat" => "MIT"
+  }
+
   def self.run(path=nil)
     unless path
       raise ArgumentError.new("Path to Gemfile or Gemspec required")
@@ -24,12 +42,37 @@ class DerivingLicense
       # See if it's installed locally, and if not add -r to call
       Bundler.with_clean_env do # This gets out of the bundler context.
         remote = /#{d.name}/.match( `BUNDLE_GEMFILE=#{path}; gem list #{d.name}` ) ? "" : "-r "      
-        print "Determining license for #{d.name}#{remote.empty? ? "" : " (remote call required)"}..."; STDOUT.flush
+        print "Determining license for #{d.name}#{remote.empty? ? "" : " (remote call required)"}..."
         @spec = eval `gem specification #{remote}#{d.name} --ruby`
       end
-      print "DONE\n"; STDOUT.flush
+      print "#{@spec.licenses.empty? ? "UNKNOWN" : "SUCCESS"}\n"
       @spec.licenses.each{ |l| licenses[l]+=1 }
     end
     licenses
+  end
+  
+  def self.describe(licenses)
+    # Print link to description of each license type, then attempt to determine 
+    # whether any notable restrictions apply (e.g. you can't sell this project, 
+    # you must include a copy of the GPL, etc)
+    unknowns = []
+    output = []
+    licenses.each do |l|
+      instances = "(#{l.last} instance#{l.last == 1 ? "" : "s"})"
+      key = @@license_aliases[l.first]
+      key ||= l.first
+      if @@license_details[key]
+        output << "#{key}: #{@@license_details[key][:name]} #{instances}[#{@@license_details[key][:link]}]"
+      else
+        unknowns << key
+      end
+    end
+    unless output.empty?
+      puts "Detected #{output.count} known license#{output.count==1 ? "" : "s"}:"
+      output.each{|o| puts o}
+    end
+    unless unknowns.empty?
+      puts "There #{unknowns.count==1 ? "is" : "are"} also #{unknowns.count} unknown license#{unknowns.count==1 ? "" : "s"}: #{unknowns.join(', ')}"
+    end
   end
 end
