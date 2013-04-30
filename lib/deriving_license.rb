e383d0d8e163bebb08bb45f66a46e90fdc0b8706
Tom Allen <tom.allen@bigcommerce.com>
2013-04-30 13:43:13 +1000

    Better output while fetching results. Call to gem list <blah> is run in a different context to user running it from cmd line, so locally installed gems aren't picked up as such.

--- lib/deriving_license.rb
+++ lib/deriving_license.rb
@@ -22,8 +22,10 @@ class DerivingLicense
     gemfile.dependencies.each do |d|
       # See if it's installed locally, and if not add -r to call
       remote = /#{d.name}/.match( `gem list #{d.name}` ) ? "" : "-r "
-
+      
+      print "Determining license for #{d.name}#{remote.empty? ? "" : " (remote call required)"}..."; STDOUT.flush
       spec = eval `gem specification #{remote}#{d.name} --ruby`
+      print "DONE\n"; STDOUT.flush
       spec.licenses.each{ |l| licenses[l]+=1 }
     end
     licenses
